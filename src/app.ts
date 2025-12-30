056bb9996db222f2ec9040d67cca5da39292a28b
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-12-30 11:34:40 +0330

    feat: add ProjectItem component
    - Render individual project data via dedicated component
    - Extend generic Component base class for consistency

--- src/app.ts
+++ src/app.ts
@@ -241,6 +241,30 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 /**
+ * ProjectItem class
+ */
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
+
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure(): void {}
+  renderContent(): void {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent =
+      this.project.people.toString();
+    this.element.querySelector('p')!.textContent = this.project.description;
+  }
+}
+
+/**
  * ProjectList class
  */
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
